c31e5c01aeb2ab2cc08ee61fac8d0fc4913e16b2
sakshi-is-committed <shakshiadarkar10@gmail.com>
2025-01-13 17:24:19 +0530

    Added some changes

--- subscribe.php
+++ subscribe.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $mail->Host = 'smtp.gmail.com'; 
             $mail->SMTPAuth = true;
             $mail->Username = 'shakshiadarkar10@gmail.com'; 
-            $mail->Password = 'lhtw eaoe cmmn aezf'; 
+            $mail->Password = ''; 
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
 
